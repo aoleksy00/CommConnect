a6909cd10d3f7daf7e5a1c8d09184d86750712ea
Alex Oleksy <aro4154>
2018-09-25 15:49:57 -0400

    Technical Coordinator Tutorial 4 TEST

---
+++ app/src/main/java/com/example/aro4154/commconnect/Activity_aro4154.java
@@ -0,0 +1,13 @@
+package com.example.aro4154.commconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_aro4154 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aro4154);
+    }
+}
